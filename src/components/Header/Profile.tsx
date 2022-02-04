1e30aec61c72807e0f70439296e239cd07afe705
John Everton <johneverton44@gmail.com>
2022-02-04 08:45:26 -0300

    responsive in header component

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,19 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { ProfileProps } from './interfaces';
 
-export function Profile() {
+export function Profile({ showProfileDate = true }: ProfileProps) {
 	return (
 		<Flex align="center">
-			<Box mr="4" textAlign="right">
-				<Text>John Everton</Text>
-				<Text color="gray.300" fontSize="small">
-					johneverton44@gmail.com
-				</Text>
-			</Box>
+			{ showProfileDate && (
+				<Box mr="4" textAlign="right">
+					<Text>John Everton</Text>
+					<Text color="gray.300" fontSize="small">
+						johneverton44@gmail.com
+					</Text>
+				</Box>
+			)}
 
-			<Avatar size="md" name="John Everton" src="https://github.com/johneverton01.png" />
+		<Avatar size="md" name="John Everton" src="https://github.com/johneverton01.png" />
 		</Flex>
 	);
 }
