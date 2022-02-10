996f2359652a39998670996116532bb795dedaa7
John Everton <johneverton44@gmail.com>
2022-02-10 08:04:59 -0300

    validate forms and fix id error in formLabel

--- src/components/Forms/Input.tsx
+++ src/components/Forms/Input.tsx
@@ -1,21 +1,25 @@
+import { forwardRef, ForwardRefRenderFunction } from 'react';
+import { FieldError } from 'react-hook-form';
 import {
-	FormControl, FormLabel, Input  as ChakraInput, InputProps as ChakraInputProps
+	FormControl, FormErrorMessage, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps
 } from "@chakra-ui/react";
 
 interface InputProps extends ChakraInputProps {
 	name: string;
 	label?: string;
+	error?: FieldError
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
-	console.log('label: ', label);
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
+	= ({ name, label, error = null, ...rest }: InputProps, ref) => {
 	return (
-		<FormControl>
-			{ !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
+		<FormControl isInvalid={!!error}>
+			{ !!label && <FormLabel id={name} htmlFor={name}>{label}</FormLabel> }
+
 			<ChakraInput
 				name={name}
 				id={name}
-				placeholder={label}
+				key={name}
 				focusBorderColor="pink.500"
 				bgColor="gray.900"
 				variant="filled"
@@ -25,8 +29,16 @@ export function Input({ name, label, ...rest }: InputProps) {
 						bgColor: "gray.900"
 					}
 				}
+				ref={ref}
 				{...rest}
 			/>
+			{ !!error && (
+				<FormErrorMessage>
+					{error.message}
+				</FormErrorMessage>
+			)}
 		</FormControl>
-		)
+	)
 }
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,82 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Head from 'next/head';
 import { Input } from '../components';
 
-export default function SignIn() {
-  return (
-    <Flex
-      w="100vw"
-      h="100vh"
-      align="center"
-      justify="center"
-    >
-      <Flex
-        as="form"
-        width="100%"
-        maxWidth={360}
-        bg="gray.800"
-        p="8"
-        borderRadius={8}
-        flexDir="column"
-      >
-      <Stack>
-        <Input name="email" type="email" label={"E-mail"} />
-        <Input name="password" type="password" label={"Senha"} />
-      </Stack>
+type SignInFormDate = {
+  email: string;
+  password: string;
+}
 
-      <Button type="submit" mt="6"  size="lg" colorScheme="pink">Entrar</Button>
+const signInFormSchema = yup.object({
+  email: yup.string().required('Email obrigatório').email('Email inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
+export default function SignIn() {
+
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+  const { errors, isSubmitting } = formState;
+
+  const handleSignIn: SubmitHandler<SignInFormDate> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
+
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Flex
+        w="100vw"
+        h="100vh"
+        align="center"
+        justify="center"
+      >
+        <Flex
+          as="form"
+          width="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDir="column"
+          onSubmit={handleSubmit(handleSignIn)}
+        >
+          <Stack>
+            <Input
+              name="email"
+              type="email"
+              label={"E-mail"}
+              error={errors.email}
+              {...register('email')}
+            />
+            <Input
+              name="password"
+              type="password"
+              label={"Senha"}
+              error={errors.password}
+              {...register('password')}
+            />
+          </Stack>
+
+          <Button
+            type="submit"
+            mt="6"
+            size="lg"
+            colorScheme="pink"
+            isLoading={isSubmitting}
+          >
+            Entrar
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   )
 }
