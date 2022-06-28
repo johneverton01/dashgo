8f9ffe7ec7e1f6828da2e5e56b9a2507bfd5f19c
John Everton <johneverton44@gmail.com>
2022-06-28 11:52:07 -0300

    create user crud pass data to api, fix pagination component

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -3,100 +3,121 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import Link from "next/link";
+import { useMutation } from 'react-query';
 import { Header, Sidebar, Input } from "../../components";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
-type CreateUserFormDate = {
-  name: string;
-  email: string;
-  password: string;
-  password_confirmation: string;
+type CreateUserFormData = {
+	name: string;
+	email: string;
+	password: string;
+	password_confirmation: string;
 }
 
 const createUserFormSchema = yup.object({
-  name: yup.string().required('Nome obrigatório'),
-  email: yup.string().required('Email obrigatório').email('Email inválido'),
-  password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracters'),
-  password_confirmation: yup.string().oneOf([
+	name: yup.string().required('Nome obrigatório'),
+	email: yup.string().required('Email obrigatório').email('Email inválido'),
+	password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracters'),
+	password_confirmation: yup.string().oneOf([
 		null, yup.ref('password'),
 	], 'As senhas não conferem')
 });
 
 export default function CreateUser() {
-	const {register, handleSubmit, formState} = useForm({
+	const router = useRouter();
+
+	const createUser = useMutation(async (user: CreateUserFormData) => {
+		const response = await api.post('users', {
+			user: {
+				...user,
+				created_at: new Date(),
+			}
+		})
+
+		return response.data.user;
+	}, {
+		onSuccess: () => {
+			queryClient.invalidateQueries('users')
+		}
+	})
+
+	const { register, handleSubmit, formState } = useForm({
 		resolver: yupResolver(createUserFormSchema)
 	});
 
 	const { errors, isSubmitting } = formState;
 
-	const handleCreateUser: SubmitHandler<CreateUserFormDate> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(values);
-  };
+	const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+		await createUser.mutateAsync(values);
+		router.push('/users');
+	};
 
 	return (
 		<Box>
 			<Header />
 			<Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-					<Sidebar />
-					<Box
-						as='form'
-						flex="1"
-						borderRadius={8}
-						bg="gray.800"
-						p={["6", "8"]}
-						onSubmit={handleSubmit(handleCreateUser)}
-					>
-						<Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
-						<Divider my="6" borderColor="gray.700" />
+				<Sidebar />
+				<Box
+					as='form'
+					flex="1"
+					borderRadius={8}
+					bg="gray.800"
+					p={["6", "8"]}
+					onSubmit={handleSubmit(handleCreateUser)}
+				>
+					<Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
+					<Divider my="6" borderColor="gray.700" />
 
-						<VStack spacing="8">
-							<SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-								<Input
-									name="name"
-									label="Nome completo"
-									error={errors.name}
-									{...register('name')}
-								/>
-								<Input
-									name="email"
-									type="email"
-									label="Email"
-									error={errors.email}
-									{...register('email')}
-								/>
-							</SimpleGrid>
+					<VStack spacing="8">
+						<SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+							<Input
+								name="name"
+								label="Nome completo"
+								error={errors.name}
+								{...register('name')}
+							/>
+							<Input
+								name="email"
+								type="email"
+								label="Email"
+								error={errors.email}
+								{...register('email')}
+							/>
+						</SimpleGrid>
 
-							<SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-								<Input
-									name="password"
-									type="password"
-									label="Senha"
-									error={errors.password}
-									{...register('password')}
-								/>
-								<Input
-									name="password_confirmation"
-									type="password"
-									label="Confirmação da senha"
-									error={errors.password_confirmation}
-									{...register('password_confirmation')}
-								/>
-							</SimpleGrid>
-						</VStack>
+						<SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+							<Input
+								name="password"
+								type="password"
+								label="Senha"
+								error={errors.password}
+								{...register('password')}
+							/>
+							<Input
+								name="password_confirmation"
+								type="password"
+								label="Confirmação da senha"
+								error={errors.password_confirmation}
+								{...register('password_confirmation')}
+							/>
+						</SimpleGrid>
+					</VStack>
 
-						<Flex mt="8" justify="flex-end">
-							<HStack spacing="4">
+					<Flex mt="8" justify="flex-end">
+						<HStack spacing="4">
 							<Link href="/users" passHref>
 								<Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
 							</Link>
-								<Button
-									type="submit"
-									colorScheme="pink"
-									isLoading={isSubmitting}
-								>Salvar</Button>
-							</HStack>
-						</Flex>
-					</Box>
+							<Button
+								type="submit"
+								colorScheme="pink"
+								isLoading={isSubmitting}
+							>Salvar</Button>
+						</HStack>
+					</Flex>
+				</Box>
 			</Flex>
 		</Box>
 	)
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs';
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs';
 import { faker } from '@faker-js/faker';
 
 type User = {
@@ -10,6 +10,9 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
     models: {
       user: Model.extend<Partial<User>>({})
     },
@@ -44,7 +47,9 @@ export function makeServer() {
         const pageEnd = pageStart + Number(per_page);
 
         const users = this.serialize(schema.all('user'))
-          .users.slice(pageStart, pageEnd)
+          .users
+          .sort((a, b) => a.name - b.name)
+          .slice(pageStart, pageEnd)
 
         return new Response(
           200,
